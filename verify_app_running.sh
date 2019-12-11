3d0c9ffe990e3a91ce3fb61a724ed5f6e1d7ea99
danielmaartens <danielrmaartens@gmail.com>
2019-12-11 17:05:13 +0200

    refactored verify_app_running.sh script

--- verify_app_running.sh
+++ verify_app_running.sh
@@ -14,7 +14,7 @@ is_app_running() {
     local __resultvar=$1
     local myresult="null"
 
-    if (exec &>/dev/tcp/127.0.0.1/8080) &>/dev/null ; then
+    if (exec &>/dev/tcp/127.0.0.1/80) &>/dev/null ; then
        local response=$(echo "running")
        eval $__resultvar="'$response'"
     fi
@@ -27,5 +27,5 @@ do
     is_app_running running
 done
 
-echo "${normal}${bold}The Chuckle Generator App is now ready ! Go to ${green} http://localhost:8080 ${normal}${bold} to check it out !"
+echo "${normal}${bold}The Chuckle Generator App is now ready ! Go to ${green} http://localhost ${normal}${bold} to check it out !"
 
