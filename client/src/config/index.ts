a03a41059333aa97c87793e7747b7a82c97d7e9d
danielmaartens <danielrmaartens@gmail.com>
2019-12-11 16:58:17 +0200

    comment production url and uncomment development url

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,5 +1,5 @@
-const DEFAULT_SERVER_HOST = 'http://167.172.22.27:4000';
-// const DEFAULT_SERVER_HOST = 'http://localhost:4000';
+// const DEFAULT_SERVER_HOST = 'http://167.172.22.27:4000';
+const DEFAULT_SERVER_HOST = 'http://localhost:4000';
 
 export default {
     SERVER_HOST: process.env.SERVER_HOST || DEFAULT_SERVER_HOST
